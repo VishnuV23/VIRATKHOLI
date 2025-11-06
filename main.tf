7fd020cd26f769170e03b4d19d000c4c3fa37e52
Vishnu D. Virgat <161711914+VishnuV23@users.noreply.github.com>
2025-11-06 17:37:35 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+region = "ap-south-1"
+}
+
+resource "aws_instance" "one" {
+  ami           = "ami-0305d3d91b9f22e84"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "dev-server"
+  }
+}
+
